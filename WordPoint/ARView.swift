5dba8e6df700e0df5a97decfe26a9b81b3e58fd7
Joseph Rock <josephrockqz@gmail.com>
2022-12-11 18:08:27 -0500

    Added plane preference enum model

--- WordPoint/ARView.swift
+++ WordPoint/ARView.swift
@@ -5,18 +5,58 @@
 //  Created by Joseph Rock on 12/11/22.
 //
 
-import SwiftUI
+import ARKit
 import RealityKit
+import SwiftUI
 
 struct ARViewContainer: UIViewRepresentable {
+    
+    // MARK: Properties
+    
+    // this will have to be dynamic based on stored value
+    private var planePreference = PlanePreference.horizontal
+    
+    // MARK: UIView Functions
     
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
         
+        // Start AR session
+        let session = arView.session
+        let config = ARWorldTrackingConfiguration()
+        switch planePreference {
+        case .horizontal:
+            config.planeDetection = [.horizontal]
+        case .vertical:
+            config.planeDetection = [.vertical]
+        case .horizontalAndVertical:
+            config.planeDetection = [.horizontal, .vertical]
+        }
+        session.run(config)
+
+        // Add coaching overlay
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        coachingOverlay.session = session
+        switch planePreference {
+        case .horizontal:
+            coachingOverlay.goal = .horizontalPlane
+        case .vertical:
+            coachingOverlay.goal = .verticalPlane
+        case .horizontalAndVertical:
+            coachingOverlay.goal = .anyPlane
+        }
+        arView.addSubview(coachingOverlay)
+
+        // Set debug options
+        #if DEBUG
+        arView.debugOptions = [.showFeaturePoints, .showAnchorOrigins, .showAnchorGeometry]
+        #endif
+        
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
-        
+
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
         
---
+++ WordPoint/Model/PlaneEnum.swift
@@ -0,0 +1,14 @@
+//
+//  PlaneEnum.swift
+//  WordPoint
+//
+//  Created by Joseph Rock on 12/11/22.
+//
+
+import Foundation
+
+enum PlanePreference {
+    case horizontal
+    case vertical
+    case horizontalAndVertical
+}
